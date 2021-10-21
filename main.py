efa0c3464589e57bb425d203b3e52351d54a68c8
Abdelaziz IMGHARNE <48071844+azizimgh@users.noreply.github.com>
2021-10-21 12:35:08 +0100

    main cleaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ################# ACCOUNT TO LISTEN TO and Slack TOKEN
-acc_id = 2949729567
-token ="xoxb-250510902278"
+acc_id = 000000 # Add the twitter account id - You can also opt for a list of accounts
+token ="xoxb-"  # Your slack token
 
 
 ################
@@ -10,6 +10,7 @@ from tweepy import API
 from config import  consumer_key ,consumer_secret ,access_token ,access_token_secret,g_daddy_key, g_daddy_sec
 import requests
 from slack import WebClient
+
 slack_client = WebClient(token)
 
 
@@ -25,20 +26,18 @@ class goDaddy_handler:
             url = "https://api.godaddy.com/v1/domains/available"
             test = requests.get(url, params={'domain':domain}, headers=self._headers)
             available = test.json()['available']
-
             if available:
                 return round(test.json()['price']/10**6,2)
             else:
                 return 0
-
         except Exception as e:
             print('An error detectec, ',e)
+            
     def send_tex(self,tex):
         try:
             response = slack_client.chat_postMessage(
                 channel="C02EJ3WLG1X",
-                text=tex
-            )
+                text=tex )
         except Exception as e:
             # You will get a SlackApiError if "ok" is False
             print(e)
@@ -52,14 +51,8 @@ class goDaddy_handler:
                 print()
                 self.send_tex('Domain available and it price on GoDaddy is'+temp+' $')
 
-myDaddy = goDaddy_handler(g_daddy_key,g_daddy_sec)
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = API(auth)
 
-
-
-
+# Twitter stream handler
 class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         words= status.text.split(' ')
@@ -71,6 +64,10 @@ class MyStreamListener(tweepy.StreamListener):
 
 
 def main():
+    myDaddy = goDaddy_handler(g_daddy_key,g_daddy_sec)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = API(auth)
     try:
         myStreamListener = MyStreamListener()
         myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
