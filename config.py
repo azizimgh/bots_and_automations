34e50c5a8f75e0d2b658dc72f81feb677d5ff80e
Abdelaziz IMGHARNE <48071844+azizimgh@users.noreply.github.com>
2021-10-21 12:30:00 +0100

    Cleaning of config file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 ##Twwitter Handler
-consumer_key = '55PL7x1ZQEoMYAJBbZ3QnKqSv'
-consumer_secret = 'opGlYpOLyfnyWQAAEzzsXcavqxPOByOAyHHA3POTkYCRyVDY2A'
-access_token = '1348577320576167936-J4EfX1provOovfND4RHI0ZwHGGWU5k'  # access token
-access_token_secret = 'y9ltQ7be03DvDmAVVDsu1fWde50OqS1Fw5ozSoMO9tIsO'  # access token secret'
+consumer_key = '$$$$$'
+consumer_secret = '$$$$$'
+access_token = '$$$$$'  
+access_token_secret = '$$$$$'
 
 ## Godaddy handler
-g_daddy_key ='dL3oyDqnuPd9_Sfqcqajjr5kjf89sPAXAsP'
-g_daddy_sec= 'Scy3usB57GJ5ndcBYwvb6L'
+g_daddy_key ='$$$$$'
+g_daddy_sec= '$$$$$'
